6f041898846124898d7cb84e37bc0bd2baf259a2
Kevin Zhuang <kevin7441@gmail.com>
2021-07-28 08:53:52 +1000

    feat: 5 types of deletion

--- lua/close_buffers.lua
+++ lua/close_buffers.lua
@@ -1,46 +1,65 @@
 local M = {}
 
-function M.clear()
-  for k, _ in pairs(package.loaded) do
-    if string.match(k, '^close_buffers') then
-      package.loaded[k] = nil
+local allowed_delete_type = {
+  nameless = true,
+  other = true,
+  hidden = true,
+  all = true,
+  this = true,
+}
+
+local function get_opts(opts)
+  local result = {}
+  result.delete_type = opts.delete_type
+  result.delete_cmd = opts.delete_cmd
+  result.force = opts.force
+
+  if allowed_delete_type[result.delete_type] == nil then
+    return
+  end
+  if result.force == true then
+    result.delete_cmd = result.delete_cmd .. '!'
+  end
+
+  return result
+end
+
+local function close_buffers(opts)
+  local buffers = vim.fn.getbufinfo({ buflisted = true })
+  local bufnr = vim.fn.bufnr('%')
+
+  local function delete_buffer(buf)
+    vim.cmd(opts.delete_cmd .. ' ' .. buf)
+  end
+
+  if opts.delete_type == 'this' then
+    delete_buffer(bufnr)
+    return
+  end
+
+  for _, buffer in pairs(buffers) do
+    if opts.delete_type == 'nameless' and buffer.name == '' then
+      delete_buffer(buffer.bufnr)
+    elseif opts.delete_type == 'other' and bufnr ~= buffer.bufnr then
+      delete_buffer(buffer.bufnr)
+    elseif opts.delete_type == 'hidden' and #buffer.windows == 0 then
+      delete_buffer(buffer.bufnr)
+    elseif opts.delete_type == 'all' then
+      delete_buffer(buffer.bufnr)
     end
   end
 end
 
-function M.close(opts)
-  local allowed_delete_type = {
-    nameless = true,
-    other = true,
-    hidden = true,
-    all = true,
-    this = true,
-  }
+function M.wipe(args)
+  args.delete_cmd = 'bwipeout'
+  local opts = get_opts(args)
+  close_buffers(opts)
+end
 
-  local delete_type = opts.delete_type
-  if allowed_delete_type[delete_type] == nil then
-    return
-  end
-
-  local delete_cmd = opts.delete_cmd
-  if delete_cmd ~= 'bdelete' and delete_cmd ~= 'bwipeout' then
-    delete_cmd = 'bdelete'
-  end
-
-  local force = opts.force or false
-  if force == true then
-    delete_cmd = delete_cmd .. '!'
-  end
-
-  local buffers = vim.fn.getbufinfo({ buflisted = true })
-  local bufnr = vim.fn.bufnr('%')
-  print(delete_type, force, delete_cmd)
-  -- for _, value in pairs(buffers) do
-  --   if bufnr ~= value.bufnr then
-  --     print(vim.inspect(value))
-  --     vim.cmd(delete_cmd ..)
-  --   end
-  -- end
+function M.delete(args)
+  args.delete_cmd = 'bdelete'
+  local opts = get_opts(args)
+  close_buffers(opts)
 end
 
 return M
