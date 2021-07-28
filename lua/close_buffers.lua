3981687ec9c831cb6cdd7ff3d7b713389128cb5a
Kevin Zhuang <kevin7441@gmail.com>
2021-07-28 16:29:37 +1000

    feat: delete buffer without messing window

--- lua/close_buffers.lua
+++ lua/close_buffers.lua
@@ -30,11 +30,39 @@ local function close_buffers(opts)
   local buffers = vim.fn.getbufinfo({ buflisted = true })
   local bufnr = vim.fn.bufnr('%')
 
+  -- delete provided buffer
   local function delete_buffer(buf)
     vim.cmd(opts.delete_cmd .. ' ' .. buf)
   end
 
+  -- focus previous buffer for all insntance of the buffer window
+  -- avoid window layout changes caused by bdelete and bwipeout
+  local function focus_prev_buffer(buf)
+    if #buffers < 2 then
+      return
+    end
+
+    local windows = vim.tbl_filter(function(win)
+      return vim.api.nvim_win_get_buf(win) == buf
+    end, vim.api.nvim_list_wins())
+
+    if #windows == 0 then
+      return
+    end
+
+    local prev_buffer_index = nil
+    for index, buffer in ipairs(buffers) do
+      if buffer.bufnr == buf then
+        prev_buffer_index = index - 1 > 0 and index - 1 or #buffers
+        for _, win in ipairs(windows) do
+          vim.api.nvim_win_set_buf(win, buffers[prev_buffer_index].bufnr)
+        end
+      end
+    end
+  end
+
   if opts.delete_type == 'this' then
+    focus_prev_buffer(bufnr)
     delete_buffer(bufnr)
     return
   end
@@ -45,6 +73,7 @@ local function close_buffers(opts)
         string.format('No write since last change for buffer %d (set force to true to override)', buffer.bufnr)
       )
     elseif opts.delete_type == 'nameless' and buffer.name == '' then
+      focus_prev_buffer(buffer.bufnr)
       delete_buffer(buffer.bufnr)
     elseif opts.delete_type == 'other' and bufnr ~= buffer.bufnr then
       delete_buffer(buffer.bufnr)
