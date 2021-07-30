0400907bc1576289de2bf33da1f20f62d638b8aa
Kevin Zhuang <kevin7441@gmail.com>
2021-07-30 15:27:58 +1000

    feat: added regex and glob support for vim commands

--- lua/close_buffers.lua
+++ lua/close_buffers.lua
@@ -14,19 +14,29 @@ function M.delete(opts)
 end
 
 --- VIM command entry function
--- @param delete_type string: Type of buffer to delete.
+-- @param args string: Command line arguments.
 -- @param command string: lua function to invoke, either 'delete' or 'wipe'.
 -- @param force string: Append bang to deletion command.
-function M.cmd(delete_type, command, force)
+function M.cmd(args, command, force)
   vim.validate({
-    type = { delete_type, 'string' },
+    type = { args, 'string' },
     command = { command, 'string' },
     force = { force, 'string', true },
   })
 
   local opts = {}
   opts.force = force == '!' and true or false
-  opts.type = delete_type
+
+  for _, value in ipairs(vim.split(args, ' ')) do
+    if string.match(value, 'glob=') then
+      opts.glob = string.sub(value, 6)
+    elseif string.match(value, 'regex=') then
+      opts.regex = string.sub(value, 7)
+    else
+      opts.type = value
+    end
+  end
+
   M[command](opts)
 end
 
--- plugin/close-buffers.vim
+++ plugin/close-buffers.vim
@@ -3,8 +3,8 @@ if exists('g:loaded_close_buffers') | finish | endif
 let s:save_cpo = &cpo
 set cpo&vim
 
-command! -bang -nargs=1 BDelete lua require('close_buffers').cmd(<q-args>, 'delete', <q-bang>)
-command! -bang -nargs=1 BWipeout lua require('close_buffers').cmd(<q-args>, 'wipe', <q-bang>)
+command! -bang -nargs=+ BDelete lua require('close_buffers').cmd(<q-args>, 'delete', <q-bang>)
+command! -bang -nargs=+ BWipeout lua require('close_buffers').cmd(<q-args>, 'wipe', <q-bang>)
 
 let &cpo = s:save_cpo
 unlet s:save_cpo
